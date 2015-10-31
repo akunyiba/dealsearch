243c622b5bb49935123c8e067432100391e2afa1
codinglikeapro <akunyiba1@gmail.com>
2015-10-31 16:49:26 +0300

    v1.0.0

---
+++ public/app/app-module.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Module
+
+// Bootstrap Material Design
+$.material.init();
+
+// App Backend
+var serviceBase = 'http://dealsearch.loc:8888/api/';
+
+var adApp = angular.module('adApp', [
+    'ngRoute',
+    'angular-toasty',
+    'adApp.ad'
+]);
+
+var adApp_ad = angular.module('adApp.ad', ['ngRoute', 'angular-toasty']);
+
+adApp_ad.config(['toastyConfigProvider', function(toastyConfigProvider) {
+    toastyConfigProvider.setConfig({
+        sound: false,
+        theme: "material",
+        timeout: 5000
+    });
+}]);
+
